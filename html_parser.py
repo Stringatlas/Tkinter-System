9e378f67d9ee99e44c56ce32775866d23c5fa777
Kevin <69014957+Stringatlas@users.noreply.github.com>
2022-06-17 12:28:16 -0700

    add temp tags

--- html_parser.py
+++ html_parser.py
@@ -6,11 +6,18 @@ class MyHTMLParser(HTMLParser):
     def __init__(self):
         self.elements = {}
         self.elements_data = {}
-        
+        self.temp_tags = []
+
     def handle_starttag(self, tag, attrs):
+        self.temp_tags += tag
+        print(self.temp_tags)
         print("Encountered a start tag:", tag)
 
     def handle_endtag(self, tag):
+        tag_occurances = [i for i, start_tag in enumerate(self.temp_tags) if tag == start_tag]
+        del self.temp_tags[tag_occurances[len(tag_occurances)]]
+
+        print(self.temp_tags)
         print("Encountered an end tag:", tag)
 
     def handle_data(self, data):
@@ -21,7 +28,7 @@ class MyHTMLParser(HTMLParser):
 
 
 if __name__ == "__main__":
-    htmlstring = '''
+    htmlstring = '''    # try using html code with bugs,
     <html>
         <head>
            <title>Test</title>
